0265727f0f9787fd956440a60830084dcf3e1c07
Adam Gurgul <adam.gurgul@intenso.pl>
2017-10-04 19:00:47 +0200

    Add Spring Boot and change port

--- src/main/java/com/github/java3wro/project/Application.java
+++ src/main/java/com/github/java3wro/project/Application.java
@@ -1,7 +1,15 @@
 package com.github.java3wro.project;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Created by agurgul on 04.10.2017.
  */
+@SpringBootApplication
 public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
