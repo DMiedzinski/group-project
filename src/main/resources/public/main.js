b278c335d64db120dee17db3f0ab18cef55978ea
Wint1993 <rejnowski@gmail.com>
2017-10-09 20:36:47 +0200

    Add Login and Register Controller

---
+++ src/main/resources/public/main.js
@@ -0,0 +1,92 @@
+var badugi = angular.module('badugi', ['ngRoute']);
+
+badugi.config(function ($routeProvider) {
+    $routeProvider
+        .when('/login',
+            {
+                controller: 'LoginController',
+                templateUrl: 'login.html'
+            })
+        .when('/register',
+            {
+                controller: 'RegisterController',
+                templateUrl: 'register.html'
+            })
+        .when('/home',
+            {
+                controller: 'ProfilController',
+                templateUrl: 'profil.html'
+            })
+        .when('/game',
+            {
+                controller: 'GameController',
+                templateUrl: 'game.html'
+            })
+        .otherwise({redirectTo: '/'});
+
+});
+
+
+badugi.controller('LoginController', function ($scope, $rootScope, $window, $http, $httpParamSerializer) {
+    console.log("Login controller");
+
+    $scope.loginFailed = false;
+    $scope.credentials = {};
+    $scope.login = function(){
+        var request = {
+            method: 'POST',
+            url: '/api/login',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            data: $httpParamSerializer($scope.credentials)
+        };
+    $http(request)
+    .then(function successCallback(response) {
+            $rootScope.authenticated = true;
+            $rootScope.user = response.data;
+            $location.path('/');
+            $scope.loginFailed = false;
+        }, function errorCallback(response) {
+            $scope.loginFailed = true;
+        });
+        $scope.credentials = {};
+
+    };
+    $scope.logout = function() {
+        $http
+            .post('/api/logout', {})
+            .success(function(response) {
+                $rootScope.authenticated = false;
+                $rootScope.user = {};
+            })
+            .error(function(err) {
+                $rootScope.authenticated = true;
+            });
+    }
+});
+
+badugi.controller("RegisterController", function($scope, $rootScope, $window, $http, $httpParamSerializer){
+    console.log("Register controller");
+    $scope.req = {};
+    $scope.register1 = function() {
+        var request = {
+            method: 'POST',
+            url: '/api/users/register',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            data: $httpParamSerializer($scope.req)
+        };
+        $http(request)
+            .then(function successCallback(response) {
+                    $rootScope.user = response.data;
+                    console.log("Register complete")
+                },
+                function errorCallback(response) {
+                    $scope.error = response.message;
+                });
+        $scope.req = {};
+
+    };
+});
