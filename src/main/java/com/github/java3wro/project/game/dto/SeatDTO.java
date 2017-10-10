e19c20337a01124992a0e25e4c94f9ab9b76c76b
wawat <wawatondel@gmail.com>
2017-10-10 19:20:45 +0200

    merged changes

---
+++ src/main/java/com/github/java3wro/project/game/dto/SeatDTO.java
@@ -0,0 +1,44 @@
+package com.github.java3wro.project.game.dto;
+
+
+/**
+ * Created by wawat on 10.10.2017.
+ */
+public class SeatDTO {
+    private String cards;
+    private String user;
+    private double money;
+    private boolean isActive;
+
+    public String getCards() {
+        return cards;
+    }
+
+    public void setCards(String cards) {
+        this.cards = cards;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+}
